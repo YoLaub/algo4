f3500e183072c1ef171bafedb1424107acee58a5
YoLaub <johngreta904@gmail.com>
2024-12-05 23:04:30 +0100

    Fin exo

--- js/eleve.js
+++ js/eleve.js
@@ -15,16 +15,39 @@ class Eleve{
     presentation(){
         alert('Bonjour je suis ' + this.nom + " " + this.prenom + ' et je suis en classe de ' + this.classe)
     }
-    newNoteMath (math){
-        this.note.math = math;
+    newNoteMath (mathNote){
+        let mathArray =  this.note.math;
+        mathArray = mathArray.push(mathNote);
     }
-    newNoteAlgo (algori){
-        this.note.algorithmique = algori;
+    newNoteAlgo (algoNote){
+        let algoArray = this.note.algorithmique;
+        algoArray = algoArray.push(algoNote)
     }
-    newNoteJs (javascript){
-        this.note.javascript = javascript;
+    newNoteJs (jsNote){
+        let jsArray = this.note.javascript;
+        jsArray = jsArray.push(jsNote)
     }
-    moyenne(){
-        alert('ma moyenne general est de ')
+    moyenneMath(){
+        let initialValue = 0;
+        let sumArray = this.note.math.reduce((i, e)=> i + e, initialValue);
+        let moyenne =  Math.round((sumArray/this.note.math.length)*100)/100;
+
+        return moyenne
     }
+    moyenneAlgo(){
+        let initialValue = 0;
+        let sumArray = this.note.algorithmique.reduce((i, e)=> i + e, initialValue);
+        let moyenne =  Math.round((sumArray/this.note.algorithmique.length)*100)/100;
+
+        return moyenne
+    }
+    moyenneJs(){
+        let initialValue = 0;
+        let sumArray = this.note.javascript.reduce((i, e)=> i + e, initialValue);
+        let moyenne =  Math.round((sumArray/this.note.javascript.length)*100)/100;
+
+        return moyenne
+    }
+
+
 }
