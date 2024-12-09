d4f8c3a7fdba8a5ddf0f5ffe8f75e242fb786e90
YoLaub <johngreta904@gmail.com>
2024-12-09 16:34:04 +0100

    finalisation et factorisation

--- js/eleve.js
+++ js/eleve.js
@@ -1,53 +1,91 @@
-class Eleve{
-    constructor (na, fi ,cl){
+class Eleve {
+  constructor(na, fi, cl) {
+    this.nom = na;
+    this.prenom = fi;
+    this.classe = cl;
+    this.note = {
+      math: [],
+      algorithmique: [],
+      javascript: [],
+    };
+  }
 
-        this.nom = na;
-        this.prenom = fi;
-        this.classe = cl;
-        this.note = {
-            math: [],
-            algorithmique: [],
-            javascript: []
-        }
-        }
-    
-    
-    presentation(){
-        alert('Bonjour je suis ' + this.nom + " " + this.prenom + ' et je suis en classe de ' + this.classe)
-    }
-    newNoteMath (mathNote){
-        let mathArray =  this.note.math;
-        mathArray = mathArray.push(mathNote);
-    }
-    newNoteAlgo (algoNote){
-        let algoArray = this.note.algorithmique;
-        algoArray = algoArray.push(algoNote)
-    }
-    newNoteJs (jsNote){
-        let jsArray = this.note.javascript;
-        jsArray = jsArray.push(jsNote)
-    }
-    moyenneMath(){
-        let initialValue = 0;
-        let sumArray = this.note.math.reduce((i, e)=> i + e, initialValue);
-        let moyenne =  Math.round((sumArray/this.note.math.length)*100)/100;
+  // -----------------------------------------------------
+  /*presentation()
 
-        return moyenne
-    }
-    moyenneAlgo(){
-        let initialValue = 0;
-        let sumArray = this.note.algorithmique.reduce((i, e)=> i + e, initialValue);
-        let moyenne =  Math.round((sumArray/this.note.algorithmique.length)*100)/100;
+    cette fonction permet à l'élève de se presenter
+    */
 
-        return moyenne
-    }
-    moyenneJs(){
-        let initialValue = 0;
-        let sumArray = this.note.javascript.reduce((i, e)=> i + e, initialValue);
-        let moyenne =  Math.round((sumArray/this.note.javascript.length)*100)/100;
+  presentation() {
+    alert(
+      "Bonjour je suis " +
+        this.nom +
+        " " +
+        this.prenom +
+        " et je suis en classe de " +
+        this.classe
+    );
+  }
 
-        return moyenne
-    }
+  // -----------------------------------------------------
+
+  // -----------------------------------------------------
+  /*newNote(param1, param2)
+
+    param1 = note
+    param2 = session
+
+    Grace à hasOwnProperty() nous verifions si la [session] est bien presente dans la liste de session. this.note[session]
+
+    cette fonction permet d'ajouter une nouvelle note dans le tableau de note
+    */
 
 
-}
+  newNote(newNote, session) {
+    if (this.note.hasOwnProperty(session)) {
+      this.note[session].push(newNote);
+    }
+  }
+
+
+
+
+  
+  // -----------------------------------------------------
+
+  // -----------------------------------------------------
+  /*moyenne(param1)
+
+  param1 = matière
+
+    cette fonction permet de generer la moyenne par matière pour l'élève du groupe
+
+    Grace à reduce(), nous additionnons le contenue de this.note[session]
+
+    Grace à hasOwnProperty() nous verifions si la [session] est bien presente dans la liste de session. this.note[session]
+
+    return = la moyenne de l'élève de la matière choisie
+
+    */
+
+  moyenne(session) {
+    let initialValue = 0;
+    let moyenne = 0;
+    let notNote = "pas";
+
+    let sumArray = this.note[session].reduce((i, e) => i + e, initialValue);
+
+    if (this.note.hasOwnProperty(session)) {
+      if (this.note[session].length == 0) {
+        alert("Vous n'avez pas encore de note en mathematique");
+        return notNote;
+      } else if (this.note[session].length == 1) {
+        moyenne = this.note[session][0];
+      } else {
+        moyenne =
+          Math.round((sumArray / this.note[session].length) * 100) / 100;
+      }
+      return moyenne;
+    }
+  }
+};
